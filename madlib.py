937dbd40313b458c146a113006ce05c68744699d
Matthew Nesbitt <matthew.nesbitt1@marist.edu>
2018-01-31 17:44:58 -0500

    Solution to Mad Libs Exersize.

---
+++ madlib.py
@@ -0,0 +1,8 @@
+def main():
+    print("Fill in the words to play MadLibs!")
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adverb = input("Enter a adverb: ")
+    place = input("Enter a place: ")
+    print("The", noun, "needed to", verb, adverb, "into the", place)
+    
