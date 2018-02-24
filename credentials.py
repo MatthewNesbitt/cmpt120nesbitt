bb0d2abc75a305eb32141bd67857c4a3671c78fd
Matthew Nesbitt <matthew.nesbitt1@marist.edu>
2018-02-23 21:03:07 -0500

    Added uppercase checker to password input

--- credentials.py
+++ credentials.py
@@ -13,9 +13,17 @@ def main():
     
     if len(password) <=8:
         print("Fool of a Took! That password is feeble! Please make it more than 8 characters.")
-        password = input("Create a new password: ")   
+        password = input("Create a new password: ")
+    elif all(x.islower() for x in password):
+        print("Fool of a Took! That password is feeble! Please include at least 1 uppercase letter..")
+        password = input("Create a new password: ")
+
 
     if len(password) >=8:
         print("The force is strong in this one…")
         print("Account configured. Your new email address is", uname + "@marist.edu")
+    elif any(x.isupper() for x in password):
+        print("The force is strong with this one...")
+        print("Account configured. Your new email address is", uname + "@marist.edu")
+
 main()
