4a69ac3ff373f0170943437c3f1d8bc481a19355
Matthew Nesbitt <matthew.nesbitt1@marist.edu>
2018-03-09 21:16:58 -0500

    Added functions for calc project.

--- calc_functions.py
+++ calc_functions.py
@@ -1,120 +1,41 @@
-from graphics import *
+# Math functions for calulator
 
-win = GraphWin('Calc', 160, 250)
+from math import *
 
-displayTextElement = Text(Point(0, 25), "")
+def addition(num1, num2):
+    result = num1 + num2
+    return float (result)
 
-calcGrid = [
-    [7, 8, 9, '+'],
-    [4, 5, 6, '-'],
-    [1, 2, 3, 'x'],
-    ['', 0, '+/-', ''],
-    ['', '', 'C', '=']
-]
-buttons = [['', '', '', ''], ['', '', '', ''], ['', '', '', ''], ['', '', '', ''], ['', '', '', '']]
+def subtraction(num1, num2):
+    result = num1 - num2
+    return float (result)
 
-def calcB(x, y, value):
-    button = Rectangle(Point(x, y), Point(x + 40, y + 40))
-    button.setFill('red')
-    button.setOutline('black')
-    button.draw(win)
-    text = Text(Point(x + 20, y + 20), value)
-    text.setTextColor('black')
-    text.setSize(15)
-    text.draw(win)
-    return button
+def multiplication(num1, num2):
+    result = num1 * num2
+    return float (result)
 
-def inner(clicked, button):
-    if clicked.getX() > button.p1.getX() and clicked.getX() < button.p2.getX():
-        if clicked.getY() > button.p1.getY() and clicked.getY() < button.p2.getY():
-            return True
-    return False
+def division(num1, num2):
+    result = num1 / num2
+    return float (result)
 
-def clickReact(clicked):
-    for m in range(5):
-        for n in range(4):
-            if clicked.getX() > buttons[m][n].p1.getX() and clicked.getX() < buttons[m][n].p2.getX():
-                if clicked.getY() > buttons[m][n].p1.getY() and clicked.getY() < buttons[m][n].p2.getY():
-                    return m, n
-    return -1, -1
+def change_sign(num1):
+    result = (num1 * -1)
+    return float (result)
 
+def square(num1):
+    result = num1 * num1
+    return float (result)
 
-def makeButtons():
-    for m in range (5):
-        for n in range(4):
-            buttons[m][n] = calcB(n * 40, m * 40 + 50, calcGrid[m][n])
+def square_root (num1):
+    result = sqrt(num1)
+    return float (result)
 
-def subtraction(op1, op2):
-    return op1 - op2
+def percent (num1):
+    result = num1 / 100
+    return float (result)
 
-def addition(op1, op2):
-    return op1 + op2
+def over_x(num1):
+    x = num1
+    result = 1 / x
+    return float (result)
 
-def multiplication(op1, op2):
-    return op1 * op2
-
-def main():
-    makeButtons()
-    displayString = ''
-    displayTextElement = Text(Point(0, 25), "")
-    displayTextElement.draw(win)
-    op1 = 0
-    op1Entered = False
-    op2 = 0
-    op2Entered = False
-    opr = ''
-    oprEntered = False
-    while True:
-        clicked = win.getMouse()
-        row, col = clickReact(clicked)
-        
-        if row >= 0:
-            userInput = calcGrid[row][col]
-            if userInput == 'C':
-                displayTextElement.undraw()
-                displayString = ''
-                displayTextElement = Text(Point(0, 25), "")
-                displayTextElement.draw(win)
-                op1Entered = False
-                op2Entered = False
-                oprEntered = False
-            else:
-                if userInput == '=':
-                    if opr == '+':
-                        answer = addition(op1, op2)
-                    elif opr == '-':
-                        answer = subtraction(op1, op2)
-                    elif opr == 'x':
-                        answer = multiplication(op1, op2)
-                    displayTextElement.undraw()
-                    displayString = str(answer).rjust(75)
-                    displayTextElement = Text(Point(0, 25), displayString)
-                    displayTextElement.draw(win)
-                    op1Entered = False
-                    op2Entered = False
-                    oprEntered = False
-                else:
-                    if op1Entered == False:
-                        op1Entered = True
-                        displayTextElement.undraw()
-                        displayString = ''
-                        displayTextElement = Text(Point(0, 25), "")
-                        displayTextElement.draw(win)
-                        op1 = userInput
-                    elif oprEntered == False:
-                        oprEntered = True
-                        opr = userInput
-                    elif op2Entered == False:
-                        op2Entered = True
-                        op2 = userInput
-                    buttons[row][col].setFill('red')
-                    displayString = (displayString + str(userInput)).rjust(75)
-                    displayTextElement.undraw()
-                    displayTextElement = Text(Point(0, 25), displayString)
-                    displayTextElement.draw(win)
-
-        for i in range(5):
-            for j in range(4):
-                if (i == row and j == col):
-                    buttons[i][j].setFill('red')
-main()
