9fc1deedd29b465ce08dc0d296ce3b5111410202
Matthew Nesbitt <matthew.nesbitt1@marist.edu>
2018-03-23 16:07:27 -0400

    Cleaned up code and added not a valid command

---
+++ ArithmeticEngine.py
@@ -0,0 +1,50 @@
+# CMPT 120 - Lab #6
+# Matt Nesbitt
+# 23-03-2018
+###
+
+def showIntro():
+    print("Welcome to the Arithmetic Engine!")
+    print("=================================\n")
+    print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
+
+def showOutro():
+    print("\nThank you for using the Arithmetic Engine…")
+    print("\nPlease come back again soon!")
+
+def doLoop():
+    while True:
+        cmd = input("What computation do you want to perform? ").lower()
+
+        if cmd in ("add", "sub", "mult", "div"):
+            num1 = int(input("Enter the first number: "))
+            num2 = int(input("Enter the second number: "))
+       
+        if cmd == "add":
+            result = num1 + num2
+            print("The result is " + str(result) + ".\n")
+
+        elif cmd == "sub":
+            result = num1 - num2
+            print("The result is " + str(result) + ".\n")
+
+        elif cmd == "mult":
+            result = num1 * num2
+            print("The result is " + str(result) + ".\n")
+
+        elif cmd == "div":
+            result = num1 // num2
+            print("The result is " + str(result) + ".\n")
+
+        elif cmd == "quit":
+            break
+        
+        else:
+            print(cmd, "is not a valid command.")
+            
+def main():
+    showIntro()
+    doLoop()
+    showOutro()
+
+main()
